ab80035eebf1859ba834304b0431ea1a00970d74
lvyang <lvyang@zhihu.com>
2018-09-01 19:21:38 +0800

    init

---
+++ go.mod
@@ -0,0 +1,7 @@
+module mimiron
+
+require (
+	github.com/astaxie/beego v1.10.1
+	golang.org/x/crypto v0.0.0-20180830192347-182538f80094 // indirect
+	gopkg.in/yaml.v2 v2.2.1 // indirect
+)
--- main.go
+++ main.go
@@ -3,9 +3,36 @@ package main
 import (
 	_ "mimiron/routers"
 	"github.com/astaxie/beego"
+	"github.com/astaxie/beego/context"
+	"net/http"
+	"html/template"
 )
 
 func main() {
+	//beego.InsertFilter("/*", beego.BeforeRouter, filterUser)
+	beego.ErrorHandler("404", page_not_found)
+	beego.ErrorHandler("401", page_note_permission)
+
 	beego.Run()
 }
 
+var filterUser = func(ctx *context.Context) {
+	_, ok := ctx.Input.Session("userLogin").(string)
+	if !ok && ctx.Request.RequestURI != "/login" {
+		ctx.Redirect(302, "/login")
+	}
+}
+
+func page_not_found(rw http.ResponseWriter, r *http.Request) {
+	t, _ := template.New("404.tpl").ParseFiles("views/404.tpl")
+	data := make(map[string]interface{})
+	data["content"] = "Page Not Found."
+	t.Execute(rw, data)
+}
+
+func page_note_permission(rw http.ResponseWriter, r *http.Request) {
+	t, _ := template.New("401.tpl").ParseFiles("views/401.tpl")
+	data := make(map[string]interface{})
+	data["content"] = "Page Not Permission."
+	t.Execute(rw, data)
+}
